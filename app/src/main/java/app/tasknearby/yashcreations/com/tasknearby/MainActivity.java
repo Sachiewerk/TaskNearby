6ad8181953b3805981ff7f493a658163c5b68f60
shilpi75 <shilpi.c75@gmail.com>
2018-06-04 14:58:18 +0530

    used strings.xml for layout

--- app/src/main/java/app/tasknearby/yashcreations/com/tasknearby/MainActivity.java
+++ app/src/main/java/app/tasknearby/yashcreations/com/tasknearby/MainActivity.java
@@ -80,6 +80,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        startActivity(new Intent(this, TaskCreatorActivity3.class));
+        finish();
 
         if (!AppUtils.hasUserSeenOnboarding(this)) {
             startActivity(new Intent(this, OnboardingActivity.class));
